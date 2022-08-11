9847785ccc3a66acd4e089c15d3f180ce4946d5b
DaniPhilo <daniel.carrasco.casado@gmail.com>
2022-08-11 20:08:45 +0200

    Added WASD keys to move snake

--- input.js
+++ input.js
@@ -13,24 +13,27 @@ const changeInputDirection = (newDirection) => {
 window.addEventListener('keydown', event => {
     switch (event.key) {
         case 'ArrowUp':
+        case 'w':
             if (lastInputDirection.y !== 0) break
             inputDirection = { x: 0, y: -1 };
             break
         case 'ArrowDown':
+        case 's':
             if (lastInputDirection.y !== 0) break
             inputDirection = { x: 0, y: 1 };
             break
         case 'ArrowRight':
+        case 'd':
             if (lastInputDirection.x !== 0) break
             inputDirection = { x: 1, y: 0 };
             break
         case 'ArrowLeft':
+        case 'a':
             if (lastInputDirection.x !== 0) break
             inputDirection = { x: -1, y: 0 };
             break
     }
-
-})
+});
 
 export {
     getInputDirection,
